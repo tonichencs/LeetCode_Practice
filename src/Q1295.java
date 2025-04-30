93b68f305df333c9672bbd7a863317a70ce84214
Toni Chen <cs15722423800@gmail.com>
2025-04-30 10:20:23 +0200

    Implement LC 1295

--- src/Q1295.java
+++ src/Q1295.java
@@ -5,7 +5,6 @@ public class Q1295 {
      * Given an array nums of integers, return how many of them contain an even number of digits.
      */
     public int findNumbers(int[] nums) {
-        // Implementation goes here
         int res = 0;
 
         for (int num : nums) {
